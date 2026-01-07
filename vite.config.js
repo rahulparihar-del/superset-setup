33fea128b6f071b10de5b9b0f44d9896bf11716b
Rahul Parihar <rahulpariharmalii@gmail.com>
2026-01-07 10:41:16 +0530

    Remove unused CSS files and update Vite configuration to change server port from 8080 to 3000

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-  port: 8080,
+  port: 3000,
   strictPort: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
       },
